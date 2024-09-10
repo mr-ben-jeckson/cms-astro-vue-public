a9ce0622dd2709aaae7a030c83fad6171736d7da
Ben Jeckson <thanhtikexaw@gmail.com>
2024-09-11 05:01:41 +0630

    client:load fix login UI done/ Gsap Animated

---
+++ src/vue/components/LoginForm.vue
@@ -0,0 +1,113 @@
+<template>
+  <div
+    ref="loginForm"
+    class="w-full h-full bg-gray-50 rounded-sm shadow-lg py-5 px-3 border border-yellow-500"
+  >
+    <div class="flex justify-center items-center">
+      <form action="" class="flex flex-col w-full">
+        <label for="fullname" class="text-sm font-bold w-full mb-2">Email *</label>
+        <input
+          type="email"
+          name="email"
+          autocomplete="off"
+          v-model="email"
+          placeholder="eg. example@email.com"
+          id="fullname"
+          class="border rounded-sm border-yellow-500 focus:border-pink-500 px-2 py-2 outline-none mb-2"
+        />
+        <label for="password" class="text-sm font-bold w-full mb-2">Password *</label>
+        <div class="relative mb-2">
+          <input
+            :type="showType ? 'text' : 'password'"
+            name="password"
+            autocomplete="off"
+            v-model="password"
+            placeholder="Password"
+            id="password"
+            class="border w-full rounded-sm border-yellow-500 focus:border-pink-500 px-2 py-2 outline-none mb-2"
+          />
+          <button type="button" @click="showPassword" class="absolute right-2 top-2">
+            <span v-if="showType">
+              <svg
+                fill="#EC4899"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 256 256"
+                id="Flat"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M50.96,37.3092A4,4,0,1,0,45.04,42.69006L67.5874,67.49231c-38.25329,21.458-54.54358,57.30957-55.24267,58.8833a3.9975,3.9975,0,0,0,0,3.249c.33886.7627,8.51269,18.88672,26.82715,37.19922,16.957,16.95606,46.13378,37.168,88.82812,37.168a122.655,122.655,0,0,0,53.05518-11.68506L205.04,218.69006A4,4,0,1,0,210.96,213.3092Zm98.14246,119.84961a35.99216,35.99216,0,0,1-48.12354-52.936ZM128,195.99182c-31.97656,0-59.88965-11.64648-82.96387-34.61719a135.61312,135.61312,0,0,1-24.59277-33.376c3.64423-7.22461,20.08423-36.8003,52.67383-54.42383L95.57446,98.278a43.99336,43.99336,0,0,0,58.94141,64.83545l20.89307,22.98242A115.06293,115.06293,0,0,1,128,195.99182Zm115.65527-66.36719c-.40625.91211-10.19238,22.584-32.37695,42.4541a4.00008,4.00008,0,1,1-5.33789-5.959,136.40463,136.40463,0,0,0,29.61621-38.12109,135.68983,135.68983,0,0,0-24.59375-33.38282C187.88867,71.64123,159.97656,59.99182,128,59.99182a121.903,121.903,0,0,0-20.02344,1.62988,4,4,0,0,1-1.31836-7.89062A129.8525,129.8525,0,0,1,128,51.99182c42.69531,0,71.87109,20.21582,88.82812,37.17578,18.31543,18.31641,26.48829,36.44531,26.82715,37.208A3.9975,3.9975,0,0,1,243.65527,129.62463Zm-108.876-36.9873a4.00025,4.00025,0,0,1,1.49609-7.85938,44.15619,44.15619,0,0,1,35.53223,39.08692,3.99885,3.99885,0,0,1-3.61035,4.35449c-.127.01172-.252.01758-.377.01758a3.99965,3.99965,0,0,1-3.97754-3.62793A36.11549,36.11549,0,0,0,134.7793,92.63733Z"
+                  ></path>
+                </g>
+              </svg>
+            </span>
+            <span v-else>
+              <svg
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke="#CCCCCC"
+                  stroke-width="0.096"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z"
+                    stroke="#EC4899"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z"
+                    stroke="#EC4899"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </g>
+              </svg>
+            </span>
+          </button>
+        </div>
+        <button type="submit" class="w-full mb-2 flex px-2 py-2 bg-pink-600 text-white font-bold rounded-sm justify-center items-center">
+            Login
+        </button>
+        <label for="register" class="text-sm w-full mb-2 text-yellow-500">
+            If you do not have an account, <a href="/auth/register" class="underline text-pink-500">create a new free</a>
+        </label>
+      </form>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { gsap } from 'gsap';
+const loginForm = ref<any>(null);
+const email = ref<string>("");
+const password = ref<string>("");
+const showType = ref<boolean>(false);
+
+const showPassword = (): void => {
+  showType.value = !showType.value;
+};
+
+onMounted(() => {
+  gsap.fromTo(loginForm.value, { translateX: 90, x: 40 }, { translateX: 0, x: 0, duration: 2 });
+});
+</script>
---
+++ src/vue/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="handleSearch" class="hidden md:flex flex-grow justify-center">
+    <div class="w-full relative max-w-md mx-3">
+      <input
+        type="text"
+        v-model="search"
+        autocomplete="off"
+        placeholder="Search..."
+        class="w-full rounded-sm px-3 py-2 text-black border-none outline-none focus:ring-2 focus:ring-yellow-500"
+      />
+      <button v-if="search" type="submit" class="absolute right-5 top-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1.5em"
+          height="1.5em"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="#e58080"
+            d="M12.048 11.074L15 14.025l-.975.975l-2.951-2.952A6.205 6.205 0 0 1 1 7.202A6.205 6.205 0 0 1 7.203 1a6.205 6.205 0 0 1 4.845 10.074m-1.382-.512a4.823 4.823 0 0 0-3.463-8.184a4.823 4.823 0 0 0-4.825 4.825a4.823 4.823 0 0 0 8.184 3.463zM8.015 4.567a1.379 1.379 0 1 0 1.823 1.824a2.758 2.758 0 1 1-5.392.812a2.757 2.757 0 0 1 3.569-2.636"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  </form>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const search = ref<string>('');
+const handleSearch = (): void => {
+    console.log(search.value);
+}
+</script>
