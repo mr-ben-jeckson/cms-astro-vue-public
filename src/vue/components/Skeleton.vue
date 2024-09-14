03ab76340ccd289332964b12f0072fdb839e8083
Ben Jeckson <thanhtikexaw@gmail.com>
2024-09-15 02:16:37 +0630

    implemeting Skeleton Component by pure SVG

---
+++ src/vue/components/Skeleton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <svg width="100%" height="500" xmlns="http://www.w3.org/2000/svg">
+      <!-- Define the gradient for the shimmering effect -->
+      <defs>
+        <linearGradient id="loading-gradient">
+          <stop offset="0%" stop-color="#f5e4e7">
+            <animate
+              attributeName="offset"
+              values="0;1"
+              dur="2s"
+              repeatCount="indefinite"
+            ></animate>
+          </stop>
+          <stop offset="50%" stop-color="#fcedf6">
+            <animate
+              attributeName="offset"
+              values="0.5;1.5"
+              dur="2s"
+              repeatCount="indefinite"
+            ></animate>
+          </stop>
+          <stop offset="100%" stop-color="#fcd4db">
+            <animate
+              attributeName="offset"
+              values="1;2"
+              dur="2s"
+              repeatCount="indefinite"
+            ></animate>
+          </stop>
+        </linearGradient>
+      </defs>
+      <rect x="0" y="0" width="100%" height="500" fill="url(#loading-gradient)"></rect>
+    </svg>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
