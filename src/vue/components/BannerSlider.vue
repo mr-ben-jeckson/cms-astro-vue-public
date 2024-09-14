9fbb20bed004baffa6f258b13a21d824ce5c59a7
Ben Jeckson <thanhtikexaw@gmail.com>
2024-09-15 01:53:58 +0630

    Implementing Home Banner Slide and Slider

---
+++ src/vue/components/BannerSlider.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script setup lang="ts">
+import Slide from './Slide.vue';
+import { ref } from 'vue';
+const slide = ref<any []>([
+    {
+        url: "",
+        header: "",
+        intro: "",
+        button: true,
+        buttonName: "",
+        buttonLink: ""
+    }
+])
+</script>
